7cad4c23665d82552d6f810ece76eb9a524c3a6f
oussamadrf <oussamadrifi107@gmail.com>
2025-10-17 23:39:42 +0100

    implemented repos api

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
-// const setTheme = (newTheme) => {
-//   window.localStorage.setItem("theme", new)
-// }
 
 
 function App() {
-  const [isLiked , setLiked] = useState(false);
-  const [likesCount, setLikesCount] = useState(375);
 
-  const handleClick = () => {
-    setLiked(prev => {
-      if (!prev) {
-        setLikesCount(prev => prev + 1)
-      } else {
-        setLikesCount(prev => prev - 1)
+  const [myRepos, setRepos] = useState([]);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      try {
+        const response = await fetch("https://api.github.com/users/oussama-drifi/repos");
+        const repos = await response.json();
+        setRepos(prev => repos.map(repo => repo.name));
+      } catch (err) {
+        setRepos(prev => ["no repos fount!"])
       }
-      return !prev
-    })
-  }
-
+    }, 3000)
+    return () => {
+      console.log("component unmounted!")
+    }
+  }, []);
+  
   return (
     <>
+      <h1>your repos</h1>
       <div className="container">
-        <span onClick={handleClick}><i className={isLiked ? "bi bi-heart-fill" : "bi bi-heart"}></i> {likesCount}</span>
+          <span className={myRepos.length !== 0 ? "d-none" : undefined}>loading...</span>
+          {myRepos && (<ul>
+            {
+              myRepos.map(repo => (<li key={repo}>{repo}</li>))
+            }
+          </ul>)}
       </div>
     </>
   )
