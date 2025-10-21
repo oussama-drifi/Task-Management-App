b7d17d8162fe2f5fb055822f419446ec83eb6082
oussamadrf <oussamadrifi107@gmail.com>
2025-10-21 20:47:31 +0100

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,28 @@ import './App.css'
 
 function App() {
 
-  // const [myRepos, setRepos] = useState([]);
-  // const [isContentLoaded, setContentLoaded] = useState(false);
+  const [myRepos, setRepos] = useState([]);
+  const [isContentLoaded, setContentLoaded] = useState(false);
 
-  // useEffect(() => {
-  //   setTimeout(async () => {
-  //     try {
-  //       const response = await fetch("https://api.github.com/users/oussama-drifi/repos");
-  //       const repos = await response.json();
-  //       setRepos(prev => repos.slice(0, 6).map(repo => repo.name));
-  //       setContentLoaded(prev => true);
-  //     } catch (err) {
-  //       setRepos(prev => ["no repos fount!"]);
-  //       setContentLoaded(prev => true);
-  //     }
-  //   }, 3000)
-  //   return () => {
-  //     console.log("component unmounted!")
-  //   }
-  // }, []);
+  useEffect(() => {
+    setTimeout(async () => {
+      try {
+        const response = await fetch("https://api.github.com/users/oussama-drifi/repos");
+        const repos = await response.json();
+        setRepos(prev => repos.slice(0, 6).map(repo => repo.name));
+        setContentLoaded(prev => true);
+      } catch (err) {
+        setRepos(prev => ["no repos fount!"]);
+        setContentLoaded(prev => true);
+      }
+    }, 3000)
+    return () => {
+      console.log("component unmounted!")
+    }
+  }, []);
 
+
+  // ======= this is for offline test
   // setTimeout(() => {
   //   setRepos(prev => [
   //     "my repo number 1",
@@ -35,44 +37,10 @@ function App() {
   //   ]);
   //   setContentLoaded(prev => true);
   // }, 1500);
-
-  const [hours, setHour] = useState(0);
-  const [minutes, setMinutes] = useState(59);
-  const [seconds, setSeconds] = useState(0);
-
-
-  // keep incrementing by a second
-  useEffect(() => {
-    const interval = setInterval(() => {
-        setSeconds(prev => prev + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [])
-
-
-  // increment minutes
-  useEffect(() => {
-    if (seconds > 59) {
-      setSeconds(0);
-      setMinutes(prev => prev + 1);
-    }
-  }, [seconds]);
-
-  // increment houres
-  useEffect(() => {
-    if (minutes > 59) {
-      setMinutes(0);
-      setHour(prev => prev + 1);
-    }
-  }, [minutes]);
-
-  const mySeconds = String(seconds).padStart(2, '0');
-  const myMinutes = String(minutes).padStart(2, '0');
-  const myHoures = String(hours).padStart(2, '0');
   
   return (
     <>
-      {/* <h1>your repos</h1>
+      <h1>your repos</h1>
       <div className="container">
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
             <span></span>
@@ -106,8 +74,8 @@ function App() {
                   </div>
                 ))
             )
-          } */}
-      {/* </div> */}
+          }
+      </div>
     </>
   )
 }
