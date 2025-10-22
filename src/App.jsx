1659bf79aae0fb6cf4e8b5de10ea05d7a1782406
oussamadrf <oussamadrifi107@gmail.com>
2025-10-22 22:59:30 +0100

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -5,72 +5,110 @@ import './App.css'
 
 function App() {
 
-  const [myRepos, setRepos] = useState([]);
+  const myContacts = [
+    {
+      image: "avatar1.png",
+      name: "arther mitchel",
+      lastMsg: "how's it going"
+    },
+    {
+      image: "avatar2.png",
+      name: "lana roze",
+      lastMsg: "can u take a day off?"
+    },
+    {
+      image: "avatar6.png",
+      name: "alex grant",
+      lastMsg: "i'm kinna busy now"
+    },
+    {
+      image: "avatar4.png",
+      name: "oscar shales",
+      lastMsg: "i'll pick you up in 20"
+    },
+    {
+      image: "avatar5.png",
+      name: "john mahone",
+      lastMsg: "i'm off duty today"
+    },
+    {
+      image: "avatar3.png",
+      name: "rick grimes",
+      lastMsg: "are u in today?"
+    },
+  ];
+  const [contacts, setContacts] = useState([]);
   const [isContentLoaded, setContentLoaded] = useState(false);
 
-  useEffect(() => {
-    setTimeout(async () => {
-      try {
-        const response = await fetch("https://api.github.com/users/oussama-drifi/repos");
-        const repos = await response.json();
-        setRepos(prev => repos.slice(0, 6).map(repo => repo.name));
-        setContentLoaded(prev => true);
-      } catch (err) {
-        setRepos(prev => ["no repos fount!"]);
-        setContentLoaded(prev => true);
-      }
-    }, 3000)
-    return () => {
-      console.log("component unmounted!")
-    }
-  }, []);
-
-
   // ======= this is for offline test
-  // setTimeout(() => {
-  //   setRepos(prev => [
-  //     "my repo number 1",
-  //     "my repo number 2",
-  //     "my repo number 3",
-  //     "my repo number 4",
-  //     "my repo number 5",
-  //   ]);
-  //   setContentLoaded(prev => true);
-  // }, 1500);
+  setTimeout(() => {
+    setContacts(prev => myContacts);
+    setContentLoaded(prev => true);
+  }, 1500);
   
   return (
     <>
-      <h1>your repos</h1>
+      <h1>Contacts</h1>
       <div className="container">
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
-            <span></span>
-            <span></span>
+            <div className="profile-image"></div>
+            <div className="content">
+              <span></span>
+              <span></span>
+            </div>
           </div>
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
-            <span></span>
-            <span></span>
+            <div className="profile-image"></div>
+            <div className="content">
+              <span></span>
+              <span></span>
+            </div>
           </div>
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
-            <span></span>
-            <span></span>
+            <div className="profile-image"></div>
+            <div className="content">
+              <span></span>
+              <span></span>
+            </div>
           </div>
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
-            <span></span>
-            <span></span>
+            <div className="profile-image">
+              
+            </div>
+            <div className="content">
+              <span></span>
+              <span></span>
+            </div>
           </div>
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
-            <span></span>
-            <span></span>
+            <div className="profile-image">
+              
+            </div>
+            <div className="content">
+              <span></span>
+              <span></span>
+            </div>
           </div>
           <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
-            <span></span>
-            <span></span>
+            <div className="profile-image">
+              
+            </div>
+            <div className="content">
+              <span></span>
+              <span></span>
+            </div>
           </div>
           {
             isContentLoaded && (
-                myRepos.map(repo => (
-                  <div key={repo} className="repo">
-                    <span>{repo}</span>
+                contacts.map(contact => (
+                  <div key={contact.name} className="contact">
+                    <div className="profile-image">
+                      <img src={new URL(`../public/images/${contact.image}`, import.meta.url).href} alt="not found" />
+                    </div>
+                    <div className="content">
+                      <span className='name'>{contact.name}</span>
+                      <span className='last-message'>{contact.lastMsg}</span>
+                    </div>
                   </div>
                 ))
             )
