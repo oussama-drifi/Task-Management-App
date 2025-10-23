d9fd9279c3deaf684d10aa70614a9654af65b01a
oussamadrf <oussamadrifi107@gmail.com>
2025-10-23 23:01:17 +0100

    added new component

---
+++ src/components/contactSkeleton.jsx
@@ -0,0 +1,13 @@
+const ContactSkeleton = ({isContentLoaded}) => {
+    return (
+        <div className={`skeleton ${isContentLoaded ? "done-loading" : "" }`}>
+            <div className="profile-image"></div>
+            <div className="content">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+export default ContactSkeleton;
