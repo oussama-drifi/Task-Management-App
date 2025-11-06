5473887a4d610afe3b26058e4a5de8eadae40e3e
oussamadrf <oussamadrifi107@gmail.com>
2025-11-06 21:05:54 +0100

    implemented nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route} from 'react-router-dom'
+import 
+      { Routes, Route, Outlet, NavLink, Link, useParams, useNavigate, Navigate }
+from 'react-router-dom'
+import NavBar from './components/navBar';
 import './App.css'
 import '@fontsource/poppins';
 // import '@fontsource/poppins/500.css';
@@ -90,21 +93,137 @@ function Contacts() {
 
 
 
-import NavBar from './components/navBar';
 
-import Search from './components/search/search';
+
+function About() {
+
+  const links = [
+    {
+      content: "blogs",
+      path: "/about/blogs",
+      icon: "bi bi-layout-text-sidebar"
+    }, {
+      content: "posts",
+      path: "/about/posts",
+      icon: "bi bi-grid"
+    }
+  ]
+
+
+
+  return (
+    <div className='about-wrapper'>
+      <h1>about page</h1>
+      <NavBar links={links}/>
+      <Outlet />
+    </div>
+  )
+}
+
+function Post() {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>post ID: {id}</h2>
+    </div>
+  )
+}
+
+
+function Posts() {
+  const posts = [
+    {
+      id: 1,
+      icon: "",
+      content: "post1",
+      path: "/about/posts/1",
+      title: "best 10 movies in 2025",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quisquam nisi officia id amet, ullam ab commodi nihil imped"
+    },
+    {
+      id: 2,
+      icon: "",
+      content: "post2",
+      path: "/about/posts/2",
+      title: "ferrari's 10 best selling cars in 2024",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quisquam nisi officia id amet, ullam ab commodi nihil imped"
+    },
+    {
+      id: 3,
+      icon: "",
+      content: "post3",
+      path: "/about/posts/3",
+      title: "how to make up to 100k $ quickly",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quisquam nisi officia id amet, ullam ab commodi nihil imped"
+    },
+  ]
+
+  return (
+    <>
+      <NavBar links={posts}/>
+      <div className="posts">
+        {
+          posts.map(post => (
+            <div key={post.content} className="post">
+              <h3>{post.title}</h3>
+              <p>{post.text}</p>
+              <Link to={`/about/posts/${post.id}`}>see more</Link>
+            </div>
+          ))
+        }
+      </div>
+      <div className="current-post">
+        <Outlet />
+      </div>
+    </>
+  )
+}
+
+
 
 function App() {
+
+  const links = [
+    {
+      content: "home",
+      path: "/",
+      icon: "bi bi-house"
+    },
+    {
+      content: "services",
+      path: "/services",
+      icon: "bi bi-easel2"
+    },
+    {
+      content: "about",
+      path: "/about",
+      icon: "bi bi-card-text"
+    },
+    {
+      content: "contact",
+      path: "/contact",
+      icon: "bi bi-envelope-at"
+    }
+  ]
+
   return (
     <div className='wrapper'>
-      <NavBar />
+      <NavBar links={links}/>
       <Routes>
           <Route path='/' element={
             <div className="wrapper">
               <h1>home</h1>
             </div>
           } />
-          <Route path='/about' element={<div><h1>about page</h1></div>} />
+          <Route path='/about' element={<About />} >
+            {/* <Route index element={<Navigate to="blogs" replace />}></Route> */}
+            <Route path='/about/blogs' element={<h2>blogs</h2>}></Route>
+            <Route path='/about/posts' element={<Posts />}>
+              {/* <Route index element={<Navigate to="/about/posts/1" replace />}></Route> */}
+              <Route path='/about/posts/:id' element={<Post />}></Route>
+            </Route>
+          </Route>
           <Route path='/contact' element={<div><h1>contact page</h1></div>} />
           <Route path='/services' element={<div><h1>services page</h1></div>} />
       </Routes>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,15 +2,16 @@ import { Link, NavLink } from "react-router-dom"
 
 import './navBar.css'
 
-const NavBar = () => {
+const NavBar = ({links}) => {
 
     return (
         <nav>
             <ul>
-                <li><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to='/'><i className="bi bi-house"></i>Home</NavLink></li>
-                <li><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to='/services'><i className="bi bi-easel2"></i>services</NavLink></li>
-                <li><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to='/about'><i className="bi bi-card-text"></i>about</NavLink></li>
-                <li><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to='/contact'><i className="bi bi-envelope-at"></i>contact</NavLink></li>
+            {
+                links.map(link => (
+                    <li key={link.content}><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to={link.path}>{link.icon && (<i className={link.icon}></i>)} {link.content}</NavLink></li>
+                ))
+            }
             </ul>
         </nav>
     )
