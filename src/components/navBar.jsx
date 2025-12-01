1c173a40983d5c12ae90463abbb15bd086717b62
oussamadrf <oussamadrifi107@gmail.com>
2025-12-01 22:38:02 +0100

    modified global styles

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,15 +1,28 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 
 import './navBar.css'
+import { useState } from "react";
 
 const NavBar = ({links}) => {
 
+    const navigate = useNavigate();
+
+    const [marginLeft, setMarginLeft] = useState(0);
+
+    const handleSelection = (path, index) => {
+        navigate(path);
+        setMarginLeft(index ? index*(115 + 5) : index*115);
+    }
+
     return (
         <nav>
-            <ul>
+            <ul className={`m-${marginLeft}`}>
             {
-                links.map(link => (
-                    <li key={link.content}><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to={link.path}>{link.icon && (<i className={link.icon}></i>)} {link.content}</NavLink></li>
+                // links.map(link => (
+                //     <li key={link.content}><NavLink className={({isActive}) => `nav-link ${isActive ? "active glass" : ""}`} to={link.path}>{link.icon && (<i className={link.icon}></i>)} {link.content}</NavLink></li>
+                // ))
+                links.map((link, index) => (
+                    <li key={link.content}><button onClick={() => handleSelection(link.content, index)} className="nav-item">{link.icon && (<i className={link.icon}></i>)} {link.content}</button></li>
                 ))
             }
             </ul>
@@ -17,4 +30,5 @@ const NavBar = ({links}) => {
     )
 
 }
+// className={`nav-link ${isActive ? "active glass" : ""}`}
 export default NavBar
