929799e507aa0628767d217a4a007e60c70b8229
oussamadrf <oussamadrifi107@gmail.com>
2026-01-01 23:49:22 +0100

    removed old imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-
-// import { BrowserRouter } from 'react-router-dom'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// )
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
