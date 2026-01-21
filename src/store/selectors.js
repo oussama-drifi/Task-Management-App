ac138478e588da91114dc9a891adf40020f25bca
oussamadrf <oussamadrifi107@gmail.com>
2026-01-21 22:49:18 +0100

    modified global state selectors

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,10 +1,8 @@
 export const selectAllTasks = (state) => state.tasks.tasks;
 
-export const selectActiveTasks = (state) => 
-    state.tasks.tasks.filter(task => !task.deleted_at);
+export const selectActiveTasks = (state) => state.tasks.tasks.filter(task => !task.deleted_at);
 
-export const selectTaskById = (state, taskId) => 
-    state.tasks.tasks.find(task => task.task_id === taskId);
+export const selectTaskById = (state, taskId) => state.tasks.tasks.find(task => task.task_id === taskId);
 
 export const selectTasksByStatus = (state, status) => 
     state.tasks.tasks.filter(task => task.status === status && !task.deleted_at);
